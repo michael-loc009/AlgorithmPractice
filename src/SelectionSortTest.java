72e676e64d040cc9eb6d2379e6e24cc08f632f94
Loc Nguyen <locdinhnguyen009@gmail.com>
2019-05-22 21:44:58 +0700

    Algorithm Version 3 change method SelectionSort from public to private

--- src/SelectionSortTest.java
+++ src/SelectionSortTest.java
@@ -12,7 +12,7 @@ import java.util.*;
  */
 public class SelectionSortTest {
 
-    public static void SelectionSort(int[] arr) {
+    private static void SelectionSort(int[] arr) {
 
         for (int i = 0; i < arr.length; i++) {
             //Store the current unsorted element
